a0194bc90ab42b92e90fca30d32114a716c52576
nazarlitvin <n.litvin41@gmail.com>
2018-07-31 20:31:53 +0300

    add icon & preloader

--- main.js
+++ main.js
@@ -7,34 +7,11 @@ const App = require('./backend');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+// let mainWindow;
+app = null;
 
 async function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1155, height: 690, frame: false});
-  mainWindow.setResizable(false);
-
-  new App(mainWindow);
-
-  // and load the index.html of the app.
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, 'frontend/build/index.html'),
-    protocol: 'file:',
-    slashes: true
-  });
-
-  mainWindow.loadURL(startUrl);
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  app = new App();
 
   // To enable the clipboard features and copy/paste shortcuts we should configure app’s menu
   const template = [
@@ -80,10 +57,12 @@ electronApp.on('window-all-closed', function () {
 electronApp.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (!app.hasWindow()) {
     createWindow();
   }
 });
+
+createWindow();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
